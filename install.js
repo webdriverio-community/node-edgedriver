dffd0f6e1d5be958fc7ba4e0c2432867d31779fa
Ping Zou <ping.zou@nagra.com>
2017-11-16 11:57:54 +0000

    Select Microsoft WebDriver to download. Support version 14, 15 and 16

--- install.js
+++ install.js
@@ -12,9 +12,20 @@ var rimraf = require('rimraf').sync
 var url = require('url')
 var util = require('util')
 var md5file = require('md5-file')
+var os = require('os')
+
 
 var libPath = path.join(__dirname, 'lib', 'edgedriver')
-var downloadUrl = 'https://download.microsoft.com/download/3/4/2/342316D7-EBE0-4F10-ABA2-AE8E0CDF36DD/MicrosoftWebDriver.exe'
+
+// Select Microsoft WebDriver to download
+var microsoftWebDriverDownloadUrls = {
+  '14393': 'https://download.microsoft.com/download/3/2/D/32D3E464-F2EF-490F-841B-05D53C848D15/MicrosoftWebDriver.exe',
+  '15063': 'https://download.microsoft.com/download/3/4/2/342316D7-EBE0-4F10-ABA2-AE8E0CDF36DD/MicrosoftWebDriver.exe',
+  '16299': 'https://download.microsoft.com/download/D/4/1/D417998A-58EE-4EFE-A7CC-39EF9E020768/MicrosoftWebDriver.exe',
+}
+var osBuildNumber = os.release().split('.')[2]
+var downloadUrl = microsoftWebDriverDownloadUrls[osBuildNumber]
+
 var platform = process.platform
 
 if (platform !== 'win32') {
