eaf63d9832ac0f1bf79f021da9e3a9259919a01d
Christian Bromann <git@bromann.dev>
2023-07-14 16:54:08 -0700

    update deps

--- src/cjs/index.ts
+++ src/cjs/index.ts
@@ -1,9 +1,13 @@
-exports.start = async function start (params: unknown) {
+async function start (params: unknown) {
   const esmPkg = await import('../index.js')
   return esmPkg.start(params)
 }
 
-exports.download = async function download (params?: string) {
+async function download (params?: string) {
   const esmPkg = await import('../index.js')
   return esmPkg.download(params)
 }
+
+exports.start = start
+exports.download = download
+module.exports = { start, download }
