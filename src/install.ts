d30b719d68c73f83f83f9289bf82dba88f129485
Christian Bromann <git@bromann.dev>
2024-05-15 11:32:50 -0700

    add logging which MS Edge path was used to detect version

--- src/install.ts
+++ src/install.ts
@@ -70,6 +70,7 @@ async function getEdgeVersionWin (edgePath: string) {
 }
 
 async function getEdgeVersionUnix (edgePath: string) {
+  log.info(`Trying to detect Microsoft Edge version from binary found at ${edgePath}`)
   const versionOutput = await new Promise<string>((resolve, reject) => cp.exec(`"${edgePath}" --version`, (err, stdout, stderr) => {
     if (err) {
       return reject(err)
